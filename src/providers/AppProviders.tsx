89696977da59d79604024c95aeda7df4d46ab72b
Bartosz Korolik <korolik.bartosz@gmail.com>
2023-04-01 16:38:35 +0200

    create logic to fetch data from db

--- src/providers/AppProviders.tsx
+++ src/providers/AppProviders.tsx
@@ -2,12 +2,15 @@ import { Provider } from 'react-redux';
 import { store } from '../Redux/store/store';
 import { GlobalStyle } from '../assets/styles/GlobalStyles/GlobalStyles';
 import { PropsWithChildren } from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 function AppProviders({ children }: PropsWithChildren) {
   return (
     <Provider store={store}>
-      <GlobalStyle />
-      {children}
+      <Router>
+        <GlobalStyle />
+        {children}
+      </Router>
     </Provider>
   );
 }
