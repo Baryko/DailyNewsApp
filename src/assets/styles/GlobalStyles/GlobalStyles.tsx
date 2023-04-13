477bed75a14d0b073dacd818a889b0b2d9305255
Bartosz Korolik <korolik.bartosz@gmail.com>
2023-04-13 20:50:20 +0200

    update styles and change closing button image

--- src/assets/styles/GlobalStyles/GlobalStyles.tsx
+++ src/assets/styles/GlobalStyles/GlobalStyles.tsx
@@ -4,13 +4,14 @@ export const GlobalStyle = createGlobalStyle`
   html {
     box-sizing: border-box;
     background-color: #f5f5f6;
-  
 
+ 
   }
   
   *, *::after, *::before {
     box-sizing: inherit;
     font-family: 'Nexa', sans-serif;
+    
   }
   
   body {
