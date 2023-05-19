90a19d9e8e1c6e97db0dee3c8675eb3389a54066
Bartosz Korolik <korolik.bartosz@gmail.com>
2023-05-19 22:20:23 +0200

    create folders to separate redux slices

--- src/Redux/store/store.tsx
+++ src/Redux/store/store.tsx
@@ -1,6 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
-import displayReducer from '../slices/displaySlice';
-import sideBarReducer from '../slices/sideBarSlice';
+import displayReducer from '../slices/displaySlice/displaySlice';
+import sideBarReducer from '../slices/sideBarSlice/sideBarSlice';
 
 export const store = configureStore({
   reducer: { display: displayReducer, barVisibility: sideBarReducer },
